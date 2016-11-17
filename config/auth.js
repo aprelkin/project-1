13326b09d6b8bf036053fa835776224808c5d8ef
dmitrz <varlamov.dimitry@gmail.com>
2016-11-17 20:55:54 +0100

    fixed callback links

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID' 		: '258669987671640', // your App ID
         'clientSecret' 	: '4dba0b143240f283305f7fd0d3ecd1b7', // your App Secret
-        'callbackURL' 	: 'http://pencilbox.de/auth/facebook/callback'
+        'callbackURL' 	: 'http://pencilbox.io/auth/facebook/callback'
     },
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'rrDHsYbxRubRePvL5dlsz48q2',
 		'consumerSecret' 	: 'khPrEcrnME7sVJcenELLWmxaN6HVXGYsZYaQgsrO7BOHmvEFUX',
-		'callbackURL' 		: 'http://pencilbox.de/auth/twitter/callback'
+		'callbackURL' 		: 'http://pencilbox.io/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '291523349662-fk95i65frnsilbjuabqjd9g7i2mp74vc.apps.googleusercontent.com',
 		'clientSecret' 	: 'XEoVZjRHW9YiwDfO2w9j9ab9',
-		'callbackURL' 	: 'http://pencilbox.de/auth/google/callback'
+		'callbackURL' 	: 'http://pencilbox.io/auth/google/callback'
 	}
 
 };
